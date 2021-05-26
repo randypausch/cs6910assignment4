4e554787682da318aec72544aaa5828a59072fc1
Manoj Bharadhwaj <79242228+randypausch@users.noreply.github.com>
2021-05-26 08:48:52 +0530

    Create tsne.py

---
+++ tsne.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import wandb
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+testing_path = "<path to test>"
+labels = np.arange(10)
+s = ["T-shirt/Top","Trouser","Pull Over","Dress","Coat","Sandal","Shirt","Sneaker","Bag","Ankle Boot"]
+test = pd.read_csv(testing_path)
+lab_test = test.label
+lab_test = lab_test.to_numpy()
+
+hidden_representation = np.load("dlpa4/HiddenRepresentations.npy")
+# print(hidden_representation.shape)
+
+tsne = TSNE(n_components=2)
+tsne_hidden = tsne.fit_transform(hidden_representation)
+plt.figure(figsize=(6, 5))
+colors = ["#476A2A", "#7851B8", "#BD3430", "#4A2D4E", "#875525", "#A83683","#4E655E","#853541","#3A3120","#535D8E"]
+for i, c, label in zip(labels, colors,s):
+    plt.scatter(tsne_hidden[lab_test == i, 0], tsne_hidden[lab_test == i, 1], c=c, label=label)
+plt.xlabel("T-SNE Component 1")
+plt.ylabel("T-SNE Component 2")
+plt.legend()
+plt.savefig("TSNE.png")
+wandb.init(project='dlpa4-manoj-shivangi', entity='shiv')
+wandb.log({"T-SNE":plt})
+ 
